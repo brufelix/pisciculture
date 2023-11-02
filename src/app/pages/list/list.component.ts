9e73958a05863767dd974cde097eaa109fd1aaf3
Bruno Dias <bruno.dias@enacom.com.br>
2023-11-02 14:17:31 -0300

    feat: adapting navigation to use view child

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
     styleUrls: ['list.component.css']
 })
 export class ListComponent {
+    @ViewChild('buttonRef', { static: true }) button: any;
 
+    constructor(private router: Router) { }
+
+    handleClick() {
+        // somente para uso do View Child
+        this.button.nativeElement.textContent = 'button clicked';
+        console.log("element obtained with view child")
+        this.router.navigate(["/"])
+    }
 }
